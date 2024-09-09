da2cfc8d03665d6718a823e61b06f6f22848700c
rickFireluck <159982081+rickFireluck@users.noreply.github.com>
2024-09-08 23:32:08 -0500

    Terminamos nuestro primer proyecto de lista de tareas en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'listaTareasApp';
+
+    listaTareas: string[] = [];
+    nuevaTarea: string = '';
+
+    //Nueva forma para inyectar servicios
+    private _tareasService = inject(TareasService);
+
+    ngOnInit(): void {
+      this.listaTareas = this._tareasService.getTareas();
+    }
+
+    agregarTarea() {
+      this._tareasService.agregarTarea(this.nuevaTarea); //agregamos la tarea al localStorage a traves del servicio
+      this.nuevaTarea = ''; //vaciamos el input
+      this.listaTareas = this._tareasService.getTareas(); //traemos toda la lista de tareas para actualizar informacion
+    }
+
+    eliminarTarea(index: number) {
+      this._tareasService.eliminarTarea(index);
+      this.listaTareas = this._tareasService.getTareas();
+    }
+
 }
---
+++ src/app/services/tareas.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TareasService {
+
+  private localStorageKey = 'listaTareas';
+
+  getTareas(): string[] {
+    return JSON.parse(localStorage.getItem(this.localStorageKey) as string) || [];
+  }
+
+  agregarTarea(tarea: string){
+    if (tarea !== '') {
+      const tareas = this.getTareas();
+    tareas.push(tarea);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tareas))
+    }
+  }
+
+  eliminarTarea(index: number){
+    const tareas = this.getTareas();
+    tareas.splice(index, 1); //elimina elementos en el indice indicado, y la cantidad de veces indicada
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tareas));
+  }
+
+  constructor() { }
+}
